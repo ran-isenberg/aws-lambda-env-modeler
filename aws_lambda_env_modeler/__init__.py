20fa091dad518b5c1ddaa17e93b943355f6f0d5b
Ran Isenberg <ran.isenberg@ranthebuilder.cloud>
2023-07-15 23:22:18 +0300

    a

--- aws_lambda_env_modeler/__init__.py
+++ aws_lambda_env_modeler/__init__.py
@@ -6,8 +6,8 @@ from .modeler import get_environment_variables, init_environment_variables
 from .types import Model
 
 __all__ = [
-    "Model",
-    "BaseModel",
-    "init_environment_variables",
-    "get_environment_variables",
+    'Model',
+    'BaseModel',
+    'init_environment_variables',
+    'get_environment_variables',
 ]
--- aws_lambda_env_modeler/types.py
+++ aws_lambda_env_modeler/types.py
@@ -2,6 +2,6 @@ from typing import TypeVar
 
 from pydantic import BaseModel
 
-Model = TypeVar("Model", bound=BaseModel)
+Model = TypeVar('Model', bound=BaseModel)
 
-__all__ = ["Model"]
+__all__ = ['Model']
