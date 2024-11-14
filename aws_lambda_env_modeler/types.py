550bd2919768f95d238dedc03bd5bd81c7f90cc9
Ran Isenberg <60175085+ran-isenberg@users.noreply.github.com>
2024-11-14 19:40:53 +0200

    feature: add python 3.13 support and deprecate 3.8 (#116)
    ---------
    
    Co-authored-by: Ran Isenberg <ran.isenberg@ranthebuilder.cloud>

--- aws_lambda_env_modeler/types.py
+++ aws_lambda_env_modeler/types.py
@@ -1,14 +1,8 @@
-import sys
-from typing import TypeVar
+from typing import Annotated, TypeVar
 
 from pydantic import BaseModel
 
 Model = TypeVar('Model', bound=BaseModel)
 
-
-if sys.version_info >= (3, 9):
-    from typing import Annotated
-else:
-    from typing_extensions import Annotated
 
 __all__ = ['Model', 'BaseModel', 'Annotated']
